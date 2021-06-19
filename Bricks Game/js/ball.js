b34fe7c4e1fd4b90719f31ee057d9edd350fc986
Blank-09 <76653144+Blank-09@users.noreply.github.com>
2021-06-19 13:15:34 +0530

    Created ball.js

---
+++ Bricks Game/js/ball.js
@@ -0,0 +1,47 @@
+class Ball {
+  constructor(game) {
+    this.gameWidth = game.gameWidth;
+    this.gameHeight = game.gameHeight;
+    this.game = game;
+    this.image = document.getElementById("myBall");
+    this.size = 12;
+    this.reset();
+  }
+
+  reset() {
+    this.position = { x: 10, y: 400 };
+    this.speed = { x: 4, y: -2 };
+  }
+
+  draw(ctx) {
+    ctx.drawImage(
+      this.image,
+      this.position.x,
+      this.position.y,
+      this.size,
+      this.size
+    );
+  }
+
+  update(deltaTime) {
+    this.position.x += this.speed.x;
+    this.position.y += this.speed.y;
+
+    if (this.position.x + this.size > this.gameWidth || this.position.x < 0) {
+      this.speed.x = -this.speed.x;
+    }
+    if (this.position.y < 0) {
+      this.speed.y = -this.speed.y;
+    }
+    if (this.position.y + this.size > this.gameHeight) {
+      this.game.lives--;
+      updateLives(this.game.lives);
+      this.reset();
+    }
+
+    if (detectCollision(this, this.game.paddle)) {
+      this.speed.y = -this.speed.y;
+      this.position.y = this.game.paddle.position.y - this.size;
+    }
+  }
+}
